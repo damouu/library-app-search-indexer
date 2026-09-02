78fdcbf5bbb0ba42303499e782163b8f59a5b97b
damouu <mouadsehbaoui@gmail.com>
2026-09-03 03:52:01 +0900

    Refactor: main.go (#LAS-8)

--- cmd/search-indexer/main.go
+++ cmd/search-indexer/main.go
@@ -2,55 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"library-app-search-indexer/internal/application"
-	"library-app-search-indexer/internal/kafka"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"library-app-search-indexer/internal/application"
 	"library-app-search-indexer/internal/config"
-	"library-app-search-indexer/internal/elasticsearch"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	cfg := config.Load()
 
-	client, err := elasticsearch.NewClient(cfg.ElasticsearchURL)
+	app, err := application.New(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Starting search indexer...")
-	fmt.Println("Kafka:", cfg.KafkaBrokers)
-	fmt.Println("Kafka topic:", cfg.KafkaTopic)
-	fmt.Println("Elasticsearch client created:", client != nil)
-
-	res, err := client.Info().Do(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Elasticsearch connected:", res)
-
-	err = elasticsearch.CreateChaptersIndex(client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Chapters index created successfully")
-
-	chapterRepository := elasticsearch.NewChapterRepository(client)
-
-	chapterIndexer := application.NewChapterIndexer(chapterRepository)
-
-	consumer, err := kafka.NewConsumer(cfg.KafkaBrokers, cfg.KafkaTopic, chapterIndexer)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Kafka consumer started...")
-
-	if err := consumer.Start(); err != nil {
+	if err := app.Run(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
