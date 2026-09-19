04b7f7ee0dfec25a467f4fba8ef3a8263fb37e3e
damouu <mouadsehbaoui@gmail.com>
2026-09-19 16:40:25 +0900

    Refactor: config.go (#LAS-18 )

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,13 +8,15 @@ import (
 
 // Config contains the configuration required by the search indexer.
 type Config struct {
-	KafkaBrokers        string
-	KafkaTopic          string
-	KafkaUsername       string
-	KafkaPassword       string
-	KafkaCAPath         string
-	ElasticsearchURL    string
-	ElasticsearchAPIKey string
+	KafkaBrokers       string
+	KafkaTopic         string
+	KafkaUsername      string
+	KafkaPassword      string
+	KafkaCAPath        string
+	OpenSearchURL      string
+	OpenSearchUsername string
+	OpenSearchPassword string
+	OpenSearchCAPath   string
 }
 
 // Load loads configuration values from environment variables.
@@ -22,12 +24,14 @@ func Load() Config {
 	_ = godotenv.Load()
 
 	return Config{
-		KafkaBrokers:        os.Getenv("KAFKA_BROKERS"),
-		KafkaTopic:          os.Getenv("KAFKA_TOPIC"),
-		KafkaUsername:       os.Getenv("KAFKA_USERNAME"),
-		KafkaPassword:       os.Getenv("KAFKA_PASSWORD"),
-		KafkaCAPath:         os.Getenv("KAFKA_CA_PATH"),
-		ElasticsearchURL:    os.Getenv("ELASTICSEARCH_URL"),
-		ElasticsearchAPIKey: os.Getenv("ELASTICSEARCH_API_KEY"),
+		KafkaBrokers:       os.Getenv("KAFKA_BROKERS"),
+		KafkaTopic:         os.Getenv("KAFKA_TOPIC"),
+		KafkaUsername:      os.Getenv("KAFKA_USERNAME"),
+		KafkaPassword:      os.Getenv("KAFKA_PASSWORD"),
+		KafkaCAPath:        os.Getenv("KAFKA_CA_PATH"),
+		OpenSearchURL:      os.Getenv("OPENSEARCH_URL"),
+		OpenSearchUsername: os.Getenv("OPENSEARCH_USERNAME"),
+		OpenSearchPassword: os.Getenv("OPENSEARCH_PASSWORD"),
+		OpenSearchCAPath:   os.Getenv("OPENSEARCH_CA_PATH"),
 	}
 }
